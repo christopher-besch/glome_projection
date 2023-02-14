17f32b9043727e1d2ef9c751c1610675179b5b4c
christopher-besch <mail@chris-besch.com>
2023-02-14 19:53:49 +0100

    implemented globe

--- src/grid.cpp
+++ src/grid.cpp
@@ -1,6 +1,8 @@
 #include "grid.h"
 #include "PoolArrays.hpp"
 
+#include <vector>
+
 #include <ArrayMesh.hpp>
 #include <Mesh.hpp>
 #include <MeshDataTool.hpp>
@@ -13,21 +15,60 @@ void Grid::_register_methods()
     register_method("_ready", &Grid::_ready);
 }
 
-void Grid::_init() {}
+void Grid::_init()
+{
+    m_radius = 20;
+
+    m_num_longitudes            = 16;
+    m_num_longitudinal_steps    = 32;
+    m_longitudinal_step_between = 2 * M_PI / m_num_longitudes;
+    m_longitudinal_step         = M_PI / m_num_longitudinal_steps;
+
+    m_num_latitudes            = 8;
+    m_num_latitudinal_steps    = 16;
+    m_latitudinal_step_between = M_PI / m_num_latitudes;
+    m_latitudinal_step         = 2 * M_PI / m_num_latitudinal_steps;
+}
 
 void Grid::_ready()
 {
-    Godot::print("hi");
     PoolVector3Array verticies {};
-    verticies.push_back(Vector3 {3, 0, 0});
-    verticies.push_back(Vector3 {0, 0, 3});
-    verticies.push_back(Vector3 {-2, -2, 0});
+    // longitudes
+    for(int lo {0}; lo < m_num_longitudes; ++lo) {
+        float la_ang, lo_ang;
+        lo_ang = lo * m_longitudinal_step_between;
+        for(int la {0}; la < m_num_longitudinal_steps; ++la) {
+            la_ang = la * m_longitudinal_step;
+            verticies.push_back(project(la_ang, lo_ang));
+            verticies.push_back(project(la_ang + m_longitudinal_step, lo_ang));
+        };
+    }
+
+    // latitudes
+    for(int la {0}; la < m_num_latitudes; ++la) {
+        float la_ang, lo_ang;
+        la_ang = la * m_latitudinal_step_between;
+        for(int lo {0}; lo < m_num_latitudinal_steps; ++lo) {
+            lo_ang = lo * m_latitudinal_step;
+            verticies.push_back(project(la_ang, lo_ang));
+            verticies.push_back(project(la_ang, lo_ang + m_latitudinal_step));
+        }
+    }
 
     ArrayMesh* arr_mesh = ArrayMesh::_new();
     Array      arrays {};
     arrays.resize(ArrayMesh::ARRAY_MAX);
     arrays[ArrayMesh::ARRAY_VERTEX] = verticies;
 
-    arr_mesh->add_surface_from_arrays(Mesh::PRIMITIVE_TRIANGLES, arrays);
+    arr_mesh->add_surface_from_arrays(Mesh::PRIMITIVE_LINES, arrays);
+
     set_mesh(arr_mesh);
+}
+
+Vector3 Grid::project(float la_ang, float lo_ang)
+{
+    // prt(la_ang << " " << lo_ang);
+    return {m_radius * std::sin(la_ang) * std::cos(lo_ang),
+            m_radius * std::sin(la_ang) * std::sin(lo_ang),
+            m_radius * std::cos(la_ang)};
 }
--- src/grid.h
+++ src/grid.h
@@ -8,14 +8,32 @@ namespace godot {
 class Grid: public MeshInstance {
     GODOT_CLASS(Grid, MeshInstance)
 
+private:
+    float m_radius;
+    // all from pole to pole
+    int   m_num_longitudes;
+    int   m_num_longitudinal_steps;
+    float m_longitudinal_step_between;
+    float m_longitudinal_step;
+
+    // like equator
+    int   m_num_latitudes;
+    int   m_num_latitudinal_steps;
+    float m_latitudinal_step_between;
+    float m_latitudinal_step;
+
 public:
-    static void _register_methods();
+    static void
+        _register_methods();
 
     Grid() {}
     ~Grid() {}
 
     void _init();
     void _ready();
+
+private:
+    Vector3 project(float la_ang, float lo_ang);
 };
 
 } // namespace godot
